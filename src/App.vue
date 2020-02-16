c6eaa6a940fe33ea9e971850c8883d15daa25654
Raphael Brand <info@raphaelbrand.de>
2020-02-16 21:09:57 +0100

    add showerthoughts

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-    <div id="app">
-        <HelloWorld />
+  <div id="app">
+    <!-- <HelloWorld /> -->
+    <div v-if="isLoaded">
+      <div v-for="showerThought in showerThoughts" :key="showerThought.data.id">
+        <p class="quote">{{ showerThought.data.title }}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from "./components/HelloWorld";
 
 export default {
-    components: {
-        HelloWorld
-    }
-}
+  data() {
+    return {
+      isLoaded: false,
+      showerThoughts: []
+    };
+  },
+  created() {
+    fetch("https://www.reddit.com/r/showerthoughts.json?limit=20")
+      .then(response => response.json())
+      .then(data => {
+        this.showerThoughts = data.data.children;
+        this.showerThoughts.shift();
+        this.showerThoughts.shift();
+        console.log(this.showerThoughts);
+        this.isLoaded = true;
+      });
+  },
+  components: {
+    // HelloWorld
+  }
+};
 </script>
